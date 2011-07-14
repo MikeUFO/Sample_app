0a8a916d9e3709994d06543dd9c8c5fba6467c86
Michael Robinson <mnrobinson@gmail.com>
2011-07-14 08:45:51 -0400

    First commit of sample app tutorial

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+#gem 'sqlite3'
+gem 'gravatar_image_tag'
+gem 'will_paginate'
+
+group :development do
+  gem 'rspec-rails'
+  gem 'annotate'
+  gem 'faker'
+end
+
+group :test do
+  gem 'webrat'
+  gem 'rspec'
+  gem 'spork'
+  gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '276cf18d1a3bad4d36c5816ac73ea38e4836a44c9f1cfb7552ecebd9ef6d8270cb60273ac0f91d5f54d201341eaa87b61280caa0e62456add4119fee3f7b3c1e'
