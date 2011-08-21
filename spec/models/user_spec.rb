f8687412001ed6f003cbcbd00345b81da163fb0a
Michael Robinson <mnrobinson@gmail.com>
2011-08-21 08:23:51 -0400

    One test still fails fix it later

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,12 @@ require 'spec_helper'
 describe User do
 
 	before(:each) do
-		@attr = {:name => "Example User", :email => "user@example.com" }
+		@attr = {
+			:name => "Example User", 
+			:email => "user@example.com",
+			:password => "foobar",
+			:password_confirmation => "foobar"
+		}
 	end
 	
 	it "should create a new instance given valid attributes" do
@@ -60,10 +65,34 @@ describe User do
 		user_with_duplicate_email.should_not be_valid
 	end
 	
-	it "shoul reject all email addresses identical up to case" do
+	it "should reject all email addresses identical up to case" do
 		upcased_email = @attr[:email].upcase
     User.create!(@attr.merge(:email => upcased_email))
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
 	end
+	
+describe "password validation" do
+	
+	it "should require a password" do 
+		User.new(@attr.merge(:password => "", :password_confirmation => "")).
+			should_not be_valid
+	end
+	
+	it "should require a matching password confirmation" do
+		User.new(@attr.merge(:password_confirmation => "invalid"))
+			should_not be_valid
+	end
+	
+	it "should reject short passwords" do
+		short = "a" * 5
+		hash = @attr.merge(:password => short, :password_confirmation => short)
+		User.new(hash).should_not_be_valid
+	end
+	
+	it "should reject long passwords" do
+		long = "a" * 41
+		hash =@attr.merge(:password => long, :password_confirmation => long)
+	end
+ end	
 end
